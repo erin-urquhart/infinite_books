a9ef2c8580e7e016526e4264dbdc5e15f57701cf
erin-urquhart <urquhart.erin@gmail.com>
2022-03-17 01:22:46 -0500

    Added images to books, added image resizing to book partial

---
+++ app/admin/books.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Book do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name, :author, :description, :publication_year, :publisher, :isbn, :price_cents,
+                :image
+  form do |f| # This is a formtastic form builder.
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :name, :author, :price_cents, presence: true
+  has_one_attached :image
 end
