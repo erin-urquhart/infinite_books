30f5e5652cb3a5bc2875a847c8ae76f261e1905f
erin-urquhart <urquhart.erin@gmail.com>
2022-03-23 14:38:58 -0500

    added styling to checkout buttons, added buy now button to cart

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   resources :books, only: %i[index show]
   resources :genres, only: %i[index show]
   resources :cart, only: %i[create destroy]
+  scope "checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
