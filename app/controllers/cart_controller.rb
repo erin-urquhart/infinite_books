ab8ff7c18e726ea3e8285a447df0f2a7c267b3b6
erin-urquhart <urquhart.erin@gmail.com>
2022-03-28 22:59:42 -0500

    made changes to cart to properly update quantities and remove items when the qty goes below 0

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,11 +1,45 @@
 class CartController < ApplicationController
   def create
+    puts "START"
+    puts session[:shopping_cart]
     item = { id: params[:id].to_i, quantity: params[:quantity].to_i }
     id = params[:id].to_i
-    unless session[:shopping_cart].include?(session[:shopping_cart][id - 1])
-      session[:shopping_cart] << item # push id onto the end of the cart array
-      redirect_to books_path
+    quantity = params[:quantity].to_i
+    found_id = false
+    session[:shopping_cart].each do |cart|
+      if cart['id'] == id
+        item['quantity']
+        cart['quantity'] = cart['quantity'] + quantity
+        found_id = true
+        if cart['quantity'] < 1
+          puts "FOUND"
+          session[:shopping_cart].delete(cart)
+        end
+      end
     end
+    if !found_id
+      session[:shopping_cart] << item
+    end
+    redirect_to books_path
+
+    # if session[:shopping_cart].include?(session[:shopping_cart][id])
+    #   puts "FOUND"
+    #   session[:shopping_cart].each do |cart|
+    #     if cart['id'] == id
+    #       item['quantity']
+    #       cart['quantity'] = cart['quantity'] + quantity
+    #       puts "MIDDLE"
+    #       puts session[:shopping_cart]
+    #     end
+    #   end
+    # else
+    #   session[:shopping_cart] << item # push id onto the end of the cart array
+    # end
+    # #session[:shopping_cart].delete_if { |check| (check['quantity'] < 1) && (puts check['quantity']) }
+    # puts "END"
+    # puts session[:shopping_cart]
+
+    # redirect_to books_path
   end
 
   def show
@@ -16,6 +50,8 @@ class CartController < ApplicationController
       id_list << item['id']
       @qty << item['quantity']
     end
+    print id_list
+    print @qty
     @books = Book.find(id_list)
   end
 
