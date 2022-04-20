d854c15c360b0a80e4e93d1dde4eedbe0b75249d
erin-urquhart <urquhart.erin@gmail.com>
2022-04-20 11:39:08 -0500

    final commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   validates :name, :author, :price_cents, presence: true
+  validates :price_cents, numericality: true
+  validates :publication_year, :isbn, numericality: {allow_nil: true}
   has_one_attached :image
   has_many :item_orders
   has_many :book_genres
